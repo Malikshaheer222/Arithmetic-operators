ba72d9b6fb174b73bc6e00b9ea9a4fb3038a8cb8
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-07-27 08:33:06 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+// Arithmetic operators
+// Addition
+let num1 = 10;
+let num2 = 5;
+console.log(num1 + num2);
+// Subtraction
+console.log(num1 - num2);
+// Multiplication
+console.log(num1 * num2);
+// Division
+console.log(num1 / num2);
+// Exponentiation
+console.log(num1 ** num2);
+//Modulus
+console.log(num1 % num2);
+///////////////////////////////////////////////
